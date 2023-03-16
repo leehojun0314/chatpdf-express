18aa1fdd38c85e2440ffbab23c59c1ed322b194f
dlghwns0314 <lhj66601234@gmail.com>
2023-03-16 23:45:35 +0900

    add test router

--- src/routes/test.js
+++ src/routes/test.js
@@ -2,11 +2,22 @@ const express = require('express');
 const getSql = require('../database/connection');
 const authenticate = require('../middleware/authenticate');
 const selectMessage = require('../model/selectMessage');
+const sendToAi = require('../utils/sendToAi');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-	const messagesResult = await selectMessage({ conversationId: 7 });
-	res.send({ messagesResult });
+	const text = req.query.text;
+	console.log('text: ', text);
+	try {
+		const messagesResult = await selectMessage({ conversationId: 7 });
+		const { messages, answer } = await sendToAi(
+			'넌 상냥한 선생님이야.',
+			text,
+		);
+		res.send({ answer });
+	} catch (error) {
+		res.send(error);
+	}
 });
 
 router.get('/:id', authenticate, (req, res) => {
