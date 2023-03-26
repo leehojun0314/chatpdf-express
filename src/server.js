d1dd0c9330d6b5f8884b491f84df46a1ab23a4db
leehojun0314 <dlghwns0314@naver.com>
2023-03-26 21:54:23 +0900

    origin log

--- src/server.js
+++ src/server.js
@@ -19,6 +19,7 @@ const allowedDomains = [
 app.use(
 	cors({
 		origin: function (origin, callback) {
+			console.log('origin: ', origin);
 			if (allowedDomains.indexOf(origin) !== -1 || !origin) {
 				callback(null, true);
 			} else {
