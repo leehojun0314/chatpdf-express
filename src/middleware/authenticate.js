b476e1a5fedca4b0496c9ae228fa4e6b31bd5f75
leehojun0314 <dlghwns0314@naver.com>
2023-04-04 15:00:07 +0900

    restore changes of commonjs

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -1,43 +1,33 @@
+const axios = require('axios');
 const configs = require('../../configs');
 
 const authenticate = (req, res, next) => {
 	const authorizationHeader = req.headers.authorization;
-	if (authorizationHeader) {
+	const token = authorizationHeader?.split(' ')[1];
+	console.log('req.headers: ', req.headers);
+	if (!token) {
 		res.status(401).json({ data: 'UnAuthorized' });
 		return;
+	} else {
+		axios
+			.get(`${configs.authenticateUrl}/api/verify?jwt=${token}`)
+			.then((response) => {
+				req.user = response.data;
+				// user ex : {
+				// 	userEmail: '',
+				// 	userName: '',
+				// 	imgUrl: '',
+				// 	type: '',
+				// 	iat: ,
+				// 	exp: ,
+				// 	iss: ''
+				//   }
+				next();
+			})
+			.catch((err) => {
+				res.status(401).json({ data: 'UnAuthorized' });
+			});
 	}
-	const token = authorizationHeader.split(' ')[1];
-	console.log('req.headers: ', req.headers);
-	req.user = {
-		userEmail: '',
-		userName: '',
-		imgUrl: '',
-		type: '',
-		iat: 123,
-		exp: 123,
-		iss: '',
-	};
-	next();
 };
-// axios
-// axios
-// 	.get(`${configs.authenticateUrl}/api/verify?jwt=${token}`)
-// 	.then((response) => {
-// 		req.user = response.data;
-// 		// user ex : {
-// 		// 	userEmail: '',
-// 		// 	userName: '',
-// 		// 	imgUrl: '',
-// 		// 	type: '',
-// 		// 	iat: ,
-// 		// 	exp: ,
-// 		// 	iss: ''
-// 		//   }
-// 		next();
-// 	})
-// 	.catch((err) => {
-// 		res.status(401).json({ data: 'UnAuthorized' });
-// 	});
-// };
 
 module.exports = authenticate;
