eaa4106114dd9c94dcfcb8331c1bc22b0d840675
leehojun0314 <dlghwns0314@naver.com>
2023-04-12 16:34:49 +0900

    test cookie 3

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -2,9 +2,8 @@ const express = require('express');
 const googleAuth = require('./google');
 const checkLogin = require('./check');
 const router = express.Router();
-router.get('/google', googleAuth);
-router.get('/check', checkLogin);
 router.get('/', (req, res) => {
+	console.log('test cookie');
 	res.cookie('test', 'testcookie', {
 		httpOnly: true,
 		secure: true,
@@ -12,4 +11,7 @@ router.get('/', (req, res) => {
 	});
 	res.send('test cookie sent');
 });
+router.get('/google', googleAuth);
+router.get('/check', checkLogin);
+
 module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,8 @@
-// const chatAi = require('./message/chatAi');
-// const chatAi2 = require('./chatAi2');
-// const chatAi3 = require('./chatAi3');
-// const getMessages = require('./getMessages');
-// const getConversations = require('./getConversations');
-// const createConversation = require('./conversation/createConversation');
 const conversation = require('./conversation/index');
 const message = require('./message/index');
-const auth = require('./auth');
+const auth = require('./auth/index');
 const test = require('./test');
 const routes = {
-	// chatAi: chatAi,
-	// chatAi2: chatAi2,
-	// chatAi3: chatAi3,
-	// getMessages: getMessages,
-	// getConversations: getConversations,
-	// createConversation: createConversation,
 	conversation: conversation,
 	message: message,
 	auth: auth,
