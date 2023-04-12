56c729786cfc6a82c2012a885dabe5daebb340e9
leehojun0314 <dlghwns0314@naver.com>
2023-04-12 16:46:33 +0900

    chage secure option

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
 	console.log('test cookie');
 	res.cookie('test', 'testcookie', {
 		httpOnly: true,
-		secure: true,
+		secure: process.env.NODE_ENV === 'production',
 		maxAge: 1000 * 60 * 60 * 2, //2 hours
 	});
 	res.send('test cookie sent');
