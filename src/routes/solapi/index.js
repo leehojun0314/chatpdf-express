3239dee6172fa00cbc1bfcf648053e00506239e2
leehojun0314 <dlghwns0314@naver.com>
2023-10-08 12:48:58 +1000

    solapi endpoint fix

--- src/routes/solapi/index.js
+++ src/routes/solapi/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authenticateSolapi = require('../../middleware/authenticateSolapi');
 const sendSolapiMessage = require('./sendSolapiMessage');
 const SendSolapiKakao = require('./sendSolapiKakao');
-router.post('/sendMessage', sendSolapiMessage);
+router.post('/sendMessages', sendSolapiMessage);
 router.post('/sendKakao', SendSolapiKakao);
 
 module.exports = router;
