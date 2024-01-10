705a1e05072c4bfd22782368c807922969daf526
leehojun0314 <dlghwns0314@naver.com>
2024-01-10 20:51:08 +1000

    change to post

--- src/routes/firebase/index.js
+++ src/routes/firebase/index.js
@@ -3,5 +3,5 @@ const setToken = require('./setToken');
 const sendNoti = require('./sendNoti');
 const router = express.Router();
 router.post('/setToken', setToken);
-router.get('/sendNoti', sendNoti);
+router.post('/sendNoti', sendNoti);
 module.exports = router;
