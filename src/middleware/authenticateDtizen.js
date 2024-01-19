a64cbd081cca4ba3d2f9b5ffcd33b935351d8cef
leehojun0314 <dlghwns0314@naver.com>
2024-01-20 02:18:21 +1000

    dtizen createQuestion fix

--- src/middleware/authenticateDtizen.js
+++ src/middleware/authenticateDtizen.js
@@ -5,6 +5,7 @@ const selectUser = require('../model/selectUser');
 const authenticateDtizen = (req, res, next) => {
 	const authorizationHeader = req.headers.authorization;
 	const token = authorizationHeader?.split(' ')[1];
+	console.log('token:', token);
 	if (!token) {
 		res.status(401).json({ data: 'UnAuthorized' });
 		return;
@@ -12,6 +13,7 @@ const authenticateDtizen = (req, res, next) => {
 		axios
 			.get(`${configs.authenticateUrl}/api/verify?jwt=${token}`)
 			.then((response) => {
+				console.log('response: ', response.data);
 				return selectUser({
 					email: response.data.user_email,
 					name: response.data.user_name,
@@ -31,10 +33,12 @@ const authenticateDtizen = (req, res, next) => {
 			})
 			.then((selectUserRes) => {
 				const userData = selectUserRes.recordset[0];
+				console.log('select user res: ', selectUserRes);
 				req.user = userData;
 				next();
 			})
 			.catch((err) => {
+				console.log('err:', err);
 				res.status(401).json({ data: 'UnAuthorized' });
 			});
 	}
--- src/utils/openai/createQuestion.js
+++ src/utils/openai/createQuestion.js
@@ -1,30 +1,32 @@
 const { default: OpenAI } = require('openai');
 const MessageGenerator = require('../generator');
 require('dotenv').config();
-const openai = new OpenAI({
-	apiKey: process.env.OPENAI_API_KEY,
-	organization: process.env.OPENAI_ORGANIZATION,
-});
+
 //volatility의 약자. 휘발성 메세지. 이전 메세지 기억못함
 async function createQuestion(content) {
 	// if (!configuration.apiKey) {
 	// 	return { message: 'no apikey presented', status: false };
 	// }
 	// const messages = MessageGenerator.messageSet(recordset);
+	const openai = new OpenAI({
+		apiKey: process.env.OPENAI_API_KEY,
+		organization: process.env.OPENAI_ORGANIZATION,
+	});
 	const prompt = MessageGenerator.presetQuestion(content);
 	try {
-		const completion = await openai.createChatCompletion({
+		// const completion = await openai.createChatCompletion({
+		// 	model: 'gpt-3.5-turbo',
+		// 	messages: [prompt],
+		// });
+		const completion = await openai.chat.completions.create({
 			model: 'gpt-3.5-turbo',
 			messages: [prompt],
 		});
-		console.log(
-			'create question token usage: ',
-			completion.data.usage.total_tokens,
-		);
-		const answer = completion.data.choices[0].message.content;
+		console.log('completion: ', completion);
+		const answer = completion.choices[0].message.content;
 		return answer;
 	} catch (error) {
-		console.log('error: ', error.response);
+		console.log('error: ', error);
 		return;
 	}
 }
