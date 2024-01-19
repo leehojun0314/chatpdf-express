7511ff93158f3f98bb14c5d8f2ff16fb0185cca6
leehojun0314 <dlghwns0314@naver.com>
2024-01-20 01:37:22 +1000

    erase configuration

--- src/utils/openai/createQuestion.js
+++ src/utils/openai/createQuestion.js
@@ -7,9 +7,9 @@ const openai = new OpenAI({
 });
 //volatility의 약자. 휘발성 메세지. 이전 메세지 기억못함
 async function createQuestion(content) {
-	if (!configuration.apiKey) {
-		return { message: 'no apikey presented', status: false };
-	}
+	// if (!configuration.apiKey) {
+	// 	return { message: 'no apikey presented', status: false };
+	// }
 	// const messages = MessageGenerator.messageSet(recordset);
 	const prompt = MessageGenerator.presetQuestion(content);
 	try {
