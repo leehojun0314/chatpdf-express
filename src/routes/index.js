811145bd59b247732d7964fe7e7f6603c20ba626
leehojun0314 <dlghwns0314@naver.com>
2024-01-19 19:47:47 +1000

    mistake fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
-// const conversation = require('./conversation/index');
-// const message = require('./message/index');
+const conversation = require('./conversation/index');
+const message = require('./message/index');
 const auth = require('./auth/index');
 // const test = require('./test');
 // const debate = require('./debate');
 const solapi = require('./solapi');
 const firebase = require('./firebase');
 const routes = {
-	// conversation: conversation,
-	// message: message,
+	conversation: conversation,
+	message: message,
 	// debate: debate,
 	auth: auth,
 	solapi: solapi,
