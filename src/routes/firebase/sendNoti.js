c582f667f9c9e599f3d91754cffa2b69daa2975c
leehojun0314 <dlghwns0314@naver.com>
2024-08-08 12:44:51 +1000

    change it to env

--- src/routes/firebase/sendNoti.js
+++ src/routes/firebase/sendNoti.js
@@ -1,48 +1,52 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../../../key/dtizen-ai-firebase-adminsdk-vb5i9-dce0a0c95b.json');
+const { config } = require('dotenv');
+config();
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT;
+const serviceAccountObj = JSON.parse(serviceAccount);
+console.log('service account: ', serviceAccountObj);
 admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccountObj),
 });
 async function sendNoti(req, res) {
-	const { FCMTokens, notiBody, notiTitle } = req.body;
-	console.log('send noti: ', req.body);
-	try {
-		if (!FCMTokens) {
-			throw new Error('Invalid tokens');
-		}
-		if (!notiBody) {
-			throw new Error('Invalid body');
-		}
-		if (!notiTitle) {
-			throw new Error('Invalid title');
-		}
+  const { FCMTokens, notiBody, notiTitle } = req.body;
+  console.log('send noti: ', req.body);
+  try {
+    if (!FCMTokens) {
+      throw new Error('Invalid tokens');
+    }
+    if (!notiBody) {
+      throw new Error('Invalid body');
+    }
+    if (!notiTitle) {
+      throw new Error('Invalid title');
+    }
 
-		const messages = [];
-		for (let token of FCMTokens) {
-			const message = {
-				apns: {
-					payload: {
-						aps: {
-							badge: 1,
-							sound: 'bingbong.aiff',
-						},
-					},
-				},
+    const messages = [];
+    for (let token of FCMTokens) {
+      const message = {
+        apns: {
+          payload: {
+            aps: {
+              badge: 1,
+              sound: 'bingbong.aiff',
+            },
+          },
+        },
 
-				notification: {
-					body: notiBody,
-					title: notiTitle,
-				},
-				token: token,
-			};
-			messages.push(message);
-		}
-		const sendRes = await admin.messaging().sendEach(messages);
+        notification: {
+          body: notiBody,
+          title: notiTitle,
+        },
+        token: token,
+      };
+      messages.push(message);
+    }
+    const sendRes = await admin.messaging().sendEach(messages);
 
-		res.status(200).send(sendRes);
-	} catch (error) {
-		console.log('error:', error);
-		res.status(500).send(error);
-	}
+    res.status(200).send(sendRes);
+  } catch (error) {
+    console.log('error:', error);
+    res.status(500).send(error);
+  }
 }
 module.exports = sendNoti;
